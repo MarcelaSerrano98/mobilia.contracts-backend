77b32c4e96135a36f626095977981cf6ecde61cb
Marcela Albarracin <lmalbarracin57@gmail.com>
2026-08-25 22:42:16 -0500

    chore: estructura inicial del proyecto con Spring Initializr

---
+++ src/main/java/com/mobilia/contracts_backend/ContractsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mobilia.contracts_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobilia/contracts_backend/ContractsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilia.contracts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
